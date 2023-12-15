8114ce3145793718ebae3cbfb29acf82d3f19788
amine20akkari <amine.akkari2@gmail.com>
2023-12-15 22:40:23 +0100

    30 min Vidéo 2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
+    #[Route('/blog', name: 'blog')]
     public function index(ArticleRepository $repo): Response
     {
         $articles = $repo->findAll();
@@ -29,9 +32,29 @@ class BlogController extends AbstractController
 
     
     #[Route("/blog/new" ,name:"blog_create")]
-    public function create()
+    public function create(Request $request, ArticleRepository $repo)
     {
-        return $this->render('blog/create.html.twig');
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+                    ->add('title', TextType::class, [
+                        'attr' => [
+                            'placeholder' => "Titre de l'article"
+                        ]
+                    ])
+                    ->add('content', TextareaType::class, [
+                        'attr' => [
+                            'placeholder' => "Contenu de l'article"
+                        ]
+                    ])
+                    ->add('image', TextType::class, [
+                        'attr' => [
+                            'placeholder' => "Contenu de l'article"
+                        ]
+                    ])
+                    ->getForm();
+        return $this->render('blog/create.html.twig',[
+            'formArticle' => $form->createView()
+        ]);
     }
 
     #[Route("/blog/{id}", name: "blog_show")]
